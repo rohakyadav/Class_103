bdbf9030a91c449b91e9aec2e0c16e6a1e98b6ec
rohakyadav <67333192+rohakyadav@users.noreply.github.com>
2021-09-21 18:20:09 -0500

    Delete data.py

--- data.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import plotly.express as px
-df = pd.read_csv("line_chart.csv")
-fig = px.line(df,x="year", y = "per capita income", color = "countrie", title = "per capita income")
-fig.show()
